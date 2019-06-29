20ce019ef461bafcac4f8e8b23863d5ee6878faa
amb seny <adrifre7@hotmail.com>
2019-06-29 09:59:53 +0200

    price deploy

--- RetailCalculator.Core.ConsoleApp/Program.cs
+++ RetailCalculator.Core.ConsoleApp/Program.cs
@@ -6,7 +6,9 @@ namespace RetailCalculator.Core.ConsoleApp
     {
         static void Main(string[] args)
         {
+            var calculator = new Calculator();
             Console.WriteLine("Welcome to retail calculator");
+            Console.WriteLine($"Item price {calculator.Price}");
             //So you can see the app
             Console.ReadLine();
         }
